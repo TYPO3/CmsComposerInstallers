07ffa89a45021bfc3ce67df46668f1eca97a9396
Helmut Hummel <info@helhum.io>
2018-03-01 12:55:04 +0100

    [BUGFIX] Expose env vars also in $_ENV

--- res/php/autoload-include.tmpl.php
+++ res/php/autoload-include.tmpl.php
@@ -1,11 +1,14 @@
 <?php
 if (!getenv('TYPO3_PATH_COMPOSER_ROOT')) {
     putenv('TYPO3_PATH_COMPOSER_ROOT=' . '{$base-dir}');
+    $_ENV['TYPO3_PATH_COMPOSER_ROOT'] = '{$base-dir}';
 }
 if (!getenv('TYPO3_PATH_ROOT')) {
     putenv('TYPO3_PATH_ROOT=' . '{$root-dir}');
+    $_ENV['TYPO3_PATH_ROOT'] = '{$root-dir}';
 }
 if (!getenv('TYPO3_PATH_WEB')) {
     putenv('TYPO3_PATH_WEB=' . '{$web-dir}');
+    $_ENV['TYPO3_PATH_WEB'] = '{$web-dir}';
 }
 // '{$composer-mode}'
