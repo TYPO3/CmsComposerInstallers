16826a7bc3db434ef9d372e34dca00d5f53e372a
Benjamin Kott <benjamin.kott@outlook.com>
2020-10-28 13:44:56 +0100

    [TASK] Streamline CI-Setup

--- src/Plugin/Core/AutoloadConnector.php
+++ src/Plugin/Core/AutoloadConnector.php
@@ -1,5 +1,4 @@
 <?php
-namespace TYPO3\CMS\Composer\Plugin\Core;
 
 /*
  * This file is part of the TYPO3 project.
@@ -13,6 +12,8 @@ namespace TYPO3\CMS\Composer\Plugin\Core;
  *
  * The TYPO3 project - inspiring people to share!
  */
+
+namespace TYPO3\CMS\Composer\Plugin\Core;
 
 use Composer\EventDispatcher\ScriptExecutionException;
 use Composer\IO\NullIO;
@@ -41,7 +42,7 @@ class AutoloadConnector
         $exec = $phpPath . '  ' . realpath($_SERVER['argv'][0]) . ' dump-autoload';
         if (0 !== ($exitCode = $process->execute($exec))) {
             $io->writeError('<error>Could not gracefully recover from typo3/cms-composer-installers upgrade. Please re-run the composer command.</error>');
-            throw new ScriptExecutionException('Error Output: '.$process->getErrorOutput(), $exitCode);
+            throw new ScriptExecutionException('Error Output: ' . $process->getErrorOutput(), $exitCode);
         }
         $io->writeError('<info>Gracefully upgraded to typo3/cms-composer-installers 1.4.x</info>', true, $io::VERBOSE);
     }
--- src/Plugin/Core/InstallerScripts/AutoloadConnector.php
+++ src/Plugin/Core/InstallerScripts/AutoloadConnector.php
@@ -1,5 +1,4 @@
 <?php
-namespace TYPO3\CMS\Composer\Plugin\Core\InstallerScripts;
 
 /*
  * This file is part of the TYPO3 project.
@@ -13,6 +12,8 @@ namespace TYPO3\CMS\Composer\Plugin\Core\InstallerScripts;
  *
  * The TYPO3 project - inspiring people to share!
  */
+
+namespace TYPO3\CMS\Composer\Plugin\Core\InstallerScripts;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
