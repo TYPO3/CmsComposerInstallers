a5b8f13ca5618addacf226e8ed88188d35a5bbc1
Christian Kuhn <lolli@schwarzbu.ch>
2024-08-13 16:27:25 +0200

    [TASK] Add PHP 8.4 compatibility

--- src/Plugin/Core/IncludeFile/BaseDirToken.php
+++ src/Plugin/Core/IncludeFile/BaseDirToken.php
@@ -48,7 +48,7 @@ class BaseDirToken implements TokenInterface
      * @param Typo3PluginConfig $typo3PluginConfig
      * @param Filesystem $filesystem
      */
-    public function __construct(IOInterface $io, Typo3PluginConfig $typo3PluginConfig, Filesystem $filesystem = null)
+    public function __construct(IOInterface $io, Typo3PluginConfig $typo3PluginConfig, ?Filesystem $filesystem = null)
     {
         $this->io = $io;
         $this->typo3PluginConfig = $typo3PluginConfig;
